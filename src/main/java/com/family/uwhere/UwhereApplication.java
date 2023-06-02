83a9fd34c530e069548b2e971c228ef4fdff40e0
김영석(KIM Youngseok)/Tech. SWAT팀/SK <08215@skcc.com>
2023-06-02 16:11:44 +0900

    first commit

---
+++ src/main/java/com/family/uwhere/UwhereApplication.java
@@ -0,0 +1,13 @@
+package com.family.uwhere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UwhereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UwhereApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/family/uwhere/UwhereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.family.uwhere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UwhereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
